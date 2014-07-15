496459296df47ed94c68ee19d215cdcfc72f99d2
Kelly Norton <kellegous@gmail.com>
2014-07-14 21:15:17 -0400

    Added render-all.py

--- render-all.py
+++ render-all.py
@@ -10,22 +10,26 @@ def main():
   base = os.path.dirname(__file__)
 
   parser = optparse.OptionParser()
-  parser.add_option('--grid-size',
-      type='int',
-      dest='grid',
-      default=20,
+  parser.add_option('--grid-sizes',
+      dest='sizes',
+      default='10,12,15,20',
       help='')
   opts, args = parser.parse_args()
+  sizes = [int(x.strip()) for x in opts.sizes.split(',')]
+
   for dir in args:
     for src in glob.glob("%s/*.jpg" % dir):
-      dst = src[:-4] + '.png'
-      print src
-      if subprocess.call([
-          os.path.join(base, 'render'),
-          '--grid-size=%d' % opts.grid,
-          src,
-          dst]) != 0:
-        return 1
+      for size in sizes:
+        dst = "%s_%d.png" % (src[:-4], size)
+        if os.path.exists(dst):
+          continue
+        print '%s (%d)' % (src, size)
+        if subprocess.call([
+            os.path.join(base, 'render'),
+            '--grid-size=%d' % size,
+            src,
+            dst]) != 0:
+          return 1
 
 if __name__ == '__main__':
   sys.exit(main())
